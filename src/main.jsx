02a5e8f43fdf54af38b238910114c5bd6a1563e5
Sigh-Lee <naskiller94@gmail.com>
2025-07-13 21:33:33 +0200

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,3 @@
-# src/main.jsx
-main_jsx_content = """
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import LicenseLogin from './components/LicenseLogin';
@@ -18,6 +16,4 @@ function App() {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
 );
-"""
-(frontend_path / "src" / "main.jsx").write_text(main_jsx_content.strip())
-);
+
